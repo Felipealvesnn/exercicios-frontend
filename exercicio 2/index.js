c029b8c0554054f57ae60b604066c606598d1519
Felipealvesnn <felipe.alvesnn@gmail.com>
2021-09-17 23:36:48 -0300

    exercicio finalizado

--- exercicio 2/index.js
+++ exercicio 2/index.js
@@ -11,23 +11,48 @@ function verificar() {
     } else {
         var fsex = document.getElementsByName('radsex');
         var idade = ano - Number(anopessoa.value);
-        
+
 
         var genero = '';
         if (fsex[0].checked) {
             genero = 'homem';
-            if (idade > 0 && idade < 10) {
+            if (idade > 0 && idade <= 10) {
                 //crianca
-               // i/mg.sr
-                img.src = "/imagem net/menino.png";
-
+                // i/mg.sr
+                img.src = "/exercicio 2/imagem net/menino.png";
+            }
+            if (idade > 10 && idade <= 20) { //adolecente
+                img.src = "/exercicio 2/imagem net/adolecente.png";
 
             }
+            if (idade > 20 && idade <= 50) {
+                img.src = "/exercicio 2/imagem net/homem.png";
+            }
+            if (idade > 50) {
+                img.src = "/exercicio 2/imagem net/velho.png";
 
+            }
+            
         } else {
             genero = 'mulher';
+            if (idade > 0 && idade <= 10) {
+                //crianca
+                // i/mg.sr
+                img.src = "/exercicio 2/imagem net/menina.png";
+            }
+            if (idade > 10 && idade <= 20) { //adolecente
+                img.src = "/exercicio 2/imagem net/adolecenta.png";
+
+            }
+            if (idade > 20 && idade <= 50) {
+                img.src = "/exercicio 2/imagem net/mulher.png";
+            }
+            if (idade > 50) {
+                img.src = "/exercicio 2/imagem net/velha.png";
+
+            }
         }
-       
+
         res.innerHTML = `idade calculada é:  ${idade} e o genero é ${genero}`
     }
 }
